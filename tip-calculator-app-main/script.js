45798ed882ae4af4da73b40f8ce26e1ba3010b34
Daniel Choma <66999833+danpth@users.noreply.github.com>
2022-03-22 04:35:34 +0100

    Added splitter

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,32 +1,96 @@
-const bill=document.getElementById("bill");
-const people=document.getElementById("people");
-const tip=document.getElementById("amountTip");
-const total=document.getElementById("amountTotal");
-const reset=document.getElementById("reset");
+const btns = document.querySelectorAll('.tip-btn-normal');
+const bill = document.getElementById('bill-input');
+const people = document.getElementById('people-input');
+const totalTip = document.getElementById('tip-total');
+const total = document.getElementById('total');
+const reset = document.querySelector('.reset-btn');
 
-bill.addEventListener('input', updateValue);
-people.addEventListener('input', updateValue);
-reset.addEventListener('click', resetValues);
+let currentPercent = 5;
+let billValue = 0;
+let peopleNumber = 1;
+let totalTipValue = 0;
+let totalValue = 0;
 
 
-function updateValue(e) {
-  console.log("ciao");
-  if(people.value.lenght<1){
-    people.value=1;
-  }  
-  total.innerHTML=`$${((bill.value)/people.value).toFixed(2)}`;
+function tipBtnClick (){
+  for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', function() {
+      
+      let currentBtn = document.querySelector('.active-btn');
+      currentBtn.className = currentBtn.className.replace('active-btn', '');
+      this.className += (' active-btn');
+      currentPercent = Number(this.id);
+      calculate();
+    });
+    
+  }
+  
 }
 
-function resetValues(f){
-  console.log("ciao");
-  bill.value=0;
-  people.value=1;
-  total.innerHTML="$0.00";
+
+
+bill.onkeyup = function(){
+  if(bill.value < 10000){
+    billValue = Number(bill.value);
+  }
+  
+  calculate();
+
+
 }
 
-function isNumberKey(evt){
-    var charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
-        return false;
-    return true;
+bill.addEventListener('focusout', function(){
+  bill.value = billValue.toFixed(2);
+  
+});
+
+people.onkeyup = function(){
+  if(/^[0-9]*$/g.test(people.value) && people.value < 1000){
+    peopleNumber = Number(people.value);
+  }
+  if(people.value.length < 0){
+    peopleNumber = 1;
+  }
+  people.value = peopleNumber;
+
+  calculate();
+
 }
+
+/*function custom(){
+  console.log('hi');
+  let currentBtn = document.querySelector('.active-btn');
+  currentBtn.className = currentBtn.className.replace('active-btn', ' ');
+}*/
+
+reset.onclick = function (){
+  bill.value = '';
+  billValue = 0;
+  people.value = '';
+  peopleNumber = 0;
+  totalTip.innerHTML = '$0.00';
+  total.innerHTML = '$0.00';
+  let currentBtn = document.querySelector('.active-btn');
+  currentBtn.className = currentBtn.className.replace('active-btn', ' ');
+  document.getElementById('5').className += ('active-btn');
+  currentPercent = 5;
+}
+
+
+function calculate(){
+  if(peopleNumber == '' || peopleNumber == 0){
+    peopleNumber = 1;
+  }
+  totalTipValue = Number(billValue * currentPercent / (peopleNumber * 100));
+  totalTip.innerHTML = "$" + totalTipValue.toFixed(2);
+  console.log(totalTipValue)
+  console.log(billValue)
+  console.log(peopleNumber)
+  console.log('-----------')
+  totalValue = ((Number(billValue)/Number(peopleNumber)) + Number(totalTipValue));
+
+  total.innerHTML = "$" + totalValue.toFixed(2);
+
+}
+
+tipBtnClick();
